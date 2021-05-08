2f428d9237b298fb81e1dc03b6b7ebccbbaf1e75
m7madmomani2 <mohammad.momani1996.kk@gmail.com>
2021-05-09 02:07:46 +0300

    add play function and fix test error

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -9,11 +9,11 @@ print("""
 
 
 def read_template(path):
-    
     with open(path, 'r') as file:
         content = file.read()
         return content.strip()
-             
+    raise FileNotFoundError
+
 
 def parse_template(content):
     cont=0
@@ -28,7 +28,6 @@ def parse_template(content):
 def merge(content ,fixed_data):
     cont=0
     data = re.findall(r"\{\}",content)
-    print(data)
     for i in fixed_data:
         content=content.replace(data[cont],i,1)
         cont+=1
@@ -38,18 +37,27 @@ def merge(content ,fixed_data):
     return content
 
 
-# Stretch Goals
-# def parse_template(content,user_input=True):
-#     counter=1
-#     fixed_data=[]
-#     data = re.findall(r"\{(.*?)\}",content)
-    # print(f'For This Game You Will Be Asked {len(data)} Qustion\n')
-    # if user_input ==True:
-    #     for i in data:
-    #         user_input=input(f'{counter}) Please Insert {i} \n')
-    #         fixed_data.append(user_input)   
-    #         counter+=1
-    # else:
-    #   return data 
-    # merge(fixed_data,content)
-    # return content,fixed_data    
+
+def play(file_path):
+    input_req=input(f'Do you want to play ? (Y/N)')
+    print(input_req.upper())
+    if input_req.upper()=="Y":
+        actual = read_template(file_path)
+
+
+        pure_text, missing_word = parse_template(actual)
+        print(pure_text)
+
+        user_input=[]
+        print("=======================================")
+        for i in missing_word:
+            element=input(f"Please Enter {i} : ")
+            user_input.append(element)
+        print("=======================================")
+        final_result=merge(pure_text, user_input)
+        print(final_result)
+
+# file_path="assets/make_me_a_video_game.txt"
+file_path="assets/dark_and_stormy_night.txt"
+play(file_path)
+
